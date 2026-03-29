f8ad2ecf4fc3a2e191dddfcc67da7ba85f93dd62
Samuel <samuelisaacandrademartins@gmail.com>
2026-03-29 10:28:57 -0300

    modified:   MergeSort.c

--- MergeSort.c
+++ MergeSort.c
@@ -7,26 +7,26 @@
 void merge(int A[], int p, int q, int r){
     int n1 = q-p;
     int n2 = r-q;
-    printf("n1 = %d; n2 = %d\n", n1, n2);
+    
     
     int L[n1+1], R[n2+1];
     int i, j, k;
     
     for(i = 0; i < n1;i++){
         L[i] = A[p+i];
-        printf("L[i] = %d\n", L[i]);
+        
     }
     
     for(j = 0; j< n2; j++){
         R[j] = A[q+j];
-        printf("R[j] = %d\n", R[j]);
+        
     }
     L[n1] = INT_MAX;
     R[n2] = INT_MAX;
     i=j=0;
     
     for (k=p; k<r; k++){
-        printf("L[i] = %d; R[j] = %d\n", L[i], R[j]);
+        
         if(L[i]<=R[j]){
             A[k] = L[i];
             i++;
@@ -40,14 +40,14 @@ void merge(int A[], int p, int q, int r){
 
 
 void mergeSort(int A[], int p, int r){
-    printf("p = %d; r = %d\n", p, r);
+    
     if (r - p >1){
         int q = (p+r)/2;
-        printf("q = %d\n", q);
+        
         mergeSort(A,p,q);
-        printf("voltei com q = %d e r = %d\n", q,r);
+        
         mergeSort(A,q,r);
-        printf("passei por MERGE com p = %d; q = %d; r = %d\n", p, q, r);
+        
         merge(A,p,q,r);
          
     }
@@ -57,12 +57,12 @@ void mergeSort(int A[], int p, int r){
 
 int main (){
 
-    int A[] = {3,1,4,2,5,7,8,6,9,0};
+    int A[tam];
     
     mergeSort(A,0,10);
     
-    for(int i=0; i<10 ;i++){
-        printf("%d\n", A[i]);
+    for(int i=0; i<tam ;i++){
+        A[i]= i;
     }
 
     return 0;
