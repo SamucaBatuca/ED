ec2e855c51ba501d4dce792ae17f84e6967a4e0c
Samuel <samuelisaacandrademartins@gmail.com>
2026-05-16 11:11:31 -0300

    modified:   LinkedListD.c 	new file:   LinkedListD.h

--- LinkedListD.h
+++ LinkedListD.h
@@ -1,3 +1,6 @@
+#ifndef LikedListD_H
+#define LikedListD_H
+
 #include <stdlib.h>
 #include <stdio.h>
 typedef struct elemento
@@ -129,81 +132,5 @@ void listarD(LinkedListD * lis){
 
 
 
-int menu();
 
-int main()
-{
-    int op = menu(), val, posi;
-    Elemento *novo;
-    LinkedListD *l = criaLista();
-    while (op != 0)
-    {
-        switch (op)
-        {
-        case 1:
-            puts("Digite o valor a ser inserido: ");
-            scanf("%d", &val);
-            novo = criaElemento(val);
-            pushInicio(l, novo);
-            novo = NULL;
-            break;
-
-        case 2:
-            novo = popInicio(l);
-            printf("popado: %d\n", novo -> valor);
-            novo = NULL;
-            break;
-        case 3:
-            puts("Digite o valor a ser inserido: ");
-            scanf("%d", &val);
-            novo = criaElemento(val);
-            pushBack(l, novo);
-            novo = NULL;
-            break;
-        case 4:
-            novo = popBack(l);
-            printf("popado: %d\n", novo -> valor);
-            break;
-        case 5:
-            puts("Digite o valor a ser inserido: ");
-            scanf("%d", &val);
-            novo = criaElemento(val);
-            puts("Digite a posicao a ser inserido (considere 0 como a primeira posicao): ");
-            scanf("%d", &posi);
-            pushPosi(l,novo,posi);
-            puts("Valor inserido com sucesso!");
-            novo = NULL;
-            break;
-        case 6:
-            puts("Digite a posicao que deseja remover: ");
-            scanf("%d", &posi);
-            novo = popPosi(l,posi);
-            if(novo == NULL){puts("Nenhum valor foi removido");}
-            else{printf("O valor removido foi: %d\n", novo -> valor);}
-            break;
-        case 7:
-            listar(l);
-            break;
-        }
-        op = menu();
-    }
-    return 0;
-}
-
-int menu()
-{
-    puts("1 - Inserir");
-    puts("2 - tirar inicio");
-    puts("3 - Inserir Fim");
-    puts("4 - Tirar Fim");
-    puts("5 - Inserir Posicao");
-    puts("6 - Remover Posicao");
-    puts("7 - Listar");
-    puts("0 - Sair");
-    puts("Digite a opcao: ");
-    int op;
-    scanf("%d", &op);
-    return op;
-}
-
-
+#endif
